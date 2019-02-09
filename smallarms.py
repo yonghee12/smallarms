fe2b6c5543a0e343f6d9d7ad9442dd45f268fa71
yonghee12 <yonghee.cheon@gmail.com>
2019-02-10 06:37:41 +0900

    adding design

--- smallarms.py
+++ smallarms.py
@@ -11,6 +11,7 @@ UPLOAD_FOLDER = './user_upload'
 ALLOWED_EXTENSIONS = set(['xlsx'])
 
 app = Flask(__name__)
+app.secret_key = "secret"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
@@ -19,35 +20,12 @@ def allowed_file(filename):
 		   filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-posts = [
-	{
-		'author': '천용희',
-		'title': 'Blog Post 1',
-		'content' : '첫번째 포스트',
-		'date_posted' : 'April 20, 2018'
-	},
-	{
-		'author' : 'John Doe',
-		'title' : 'Blog Post 2',
-		'content' : '두번째 포스트',
-		'date_posted' : 'April 20, 2018'
-	}
-]
-
-@app.route("/home")
-def home():
-	return render_template('home.html', posts=posts)
-
-@app.route("/about")
-def about():
-	return render_template('about.html', page_title='About')
-
 @app.route("/")
 def root():
-	return redirect(url_for('upload_file'))
+	return redirect(url_for('reward'))
 
-@app.route("/reward", methods=['GET', 'POST'])
-def upload_file():
+@app.route("/test", methods=['GET', 'POST'])
+def test():
 	global DAY
 	if request.method == 'POST':
 		# check if the post request has the file part
@@ -68,7 +46,7 @@ def upload_file():
 			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 			# return redirect(url_for('upload_file', filename=filename))
 			return redirect(url_for('run_hr'))
-	return render_template('hcc_daily_rewards.html', page_title="hcc_rewards", DAY=DAY)
+	return render_template('reward_v1.html', page_title="hcc_rewards", DAY=DAY)
 
 @app.route("/reward_result", methods=['GET', 'POST'])
 def run_hr():
@@ -76,6 +54,30 @@ def run_hr():
 	print(DAY)
 	return render_template('reward_result.html', hr=hr, DAY=DAY)
 
+@app.route("/reward", methods=['GET', 'POST'])
+def reward():
+	global DAY
+	if request.method == 'POST':
+		# check if the post request has the file part
+		if 'file' not in request.files:
+			flash('No file part')
+			return redirect(request.url)
+		file = request.files['file']
+		# if user does not select file, browser also
+		# submit an empty part without filename
+		if file.filename == '':
+			flash('No selected file')
+			return redirect(request.url)
+		if file and allowed_file(file.filename):
+			filename = secure_filename(file.filename)
+			DAY = filename[-11:-5]
+			print(filename, DAY)
+			filename = "rw_report.xlsx"
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			# return redirect(url_for('upload_file', filename=filename))
+			return redirect(url_for('run_hr'))
+	# return render_template('hcc_daily_rewards.html', page_title="hcc_rewards", DAY=DAY)
+	return render_template('reward_v2.html', page_title="hcc_rewards", DAY=DAY)
 # hr.run("190207")
 
 if __name__ == "__main__":
