8762fda153517d190e167f4c6f4f77222aca1043
yonghee <yonghee@qpmac1ui-MacBook-Pro.local>
2019-06-17 12:37:14 +0900

    api time sorted

--- api_func.py
+++ api_func.py
@@ -4,8 +4,22 @@ import shutil
 import json
 import youtube_dl2 as youtube_dl
 
+def get_time_sorted_filelist(filelist):
+    filelist_with_time = []
+    for filename in filelist:
+        created_time = os.stat('files/' + filename).st_ctime
+        print(filename, created_time)
+        filelist_with_time.append((created_time, filename))
+    filelist_with_time.sort(key = lambda element : element[0])
+    return filelist_with_time
+
 def get_descriptions(urls):
-    os.mkdir('files')
+    try:
+        os.mkdir('files')
+    except FileExistsError:
+        print("FILES FOLDER NOT DELETED")
+        shutil.rmtree('files')
+        os.mkdir('files')
 
     ydl_opts = {
         'writedescription' : True,
@@ -15,15 +29,19 @@ def get_descriptions(urls):
         ydl.download(urls)
 
     filelist = os.listdir('files')
+    filelist = get_time_sorted_filelist(filelist)
     print('filelist: ', filelist, '\n')
     data = []
-    for filename in filelist:
+    for fileinfo in filelist:
+        filename = fileinfo[1]
         f = open('files/' + filename, 'r')
         text = f.read()
         print('text: ', text, '\n')
         data.append(text)
         f.close()
-    print('-----------\n', 'LENGTH', len(data), '-----------\n')
+    print('-----------\n', 'LENGTH', len(data), '\n-----------\n')
+
+    # 생성된 폴더를 삭제
     try:
         shutil.rmtree('files')
     except OSError as e:
